e68fd4a0ce45597a24a063c4434958ed923641d5
CJINBNH <alex_egorov_89@mail.ru>
2022-08-25 21:58:53 +0400

    Задали двумерный массив

--- task_001/Program.cs
+++ task_001/Program.cs
@@ -1,5 +1,6 @@
 ﻿// Задать двумерный массив
 
+Console.Clear();
 Console.WriteLine("Задайте двумерный массив m x n");
 Console.WriteLine("Введите число m: ");
 int m = Convert.ToInt32(Console.ReadLine());
@@ -9,10 +10,10 @@ int n = Convert.ToInt32(Console.ReadLine());
 int[,] matrix = new int[m, n];
 for (int i = 0; i < m; i++)
 {
-    for (int j = 0; j < n; j++);
+    for (int k = 0; k < n; k++)
     {
-        matrix[i, j] = new Random().Next(-100, 101);
-        Console.WriteLine(matrix[i,j] + " ");
+        matrix[i, k] = new Random().Next(-100, 101);
+        Console.Write(matrix[i, k] + " ");
     }
     Console.WriteLine();
 }

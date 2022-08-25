0e12bb8ac438f9fab581ca484eee634664466674
CJINBNH <alex_egorov_89@mail.ru>
2022-08-25 22:14:30 +0400

    Находим сумму элементов массива, находящихся на главной диагонали

--- task_004/Program.cs
+++ task_004/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать двумерный массив. Найти сумму элементов, находящихся на главной диагонали.
+
+Console.Clear();
+Console.WriteLine("Задайте двумерный массив m x n");
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] matrix = new double[m, n];
+double res = 0;
+for (int i = 0; i < m; i++)
+{
+    for (int k = 0; k < n; k++)
+    {
+        matrix[i, k] = new Random().Next(-100, 101);
+        if (i == k)
+        {
+        res = res + matrix[i, k];
+        }
+        Console.Write(matrix[i, k] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine($"Сумма элементов, находящихся на главной диагонали массива равна {res}");

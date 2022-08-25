99a4febd0a00748781f956fcf7934454b5bd0ebb
CJINBNH <alex_egorov_89@mail.ru>
2022-08-25 22:02:19 +0400

    Задали двумерный массив размера m на n, каждый элемент в массиве находится по формуле A = m+n

--- task_002/Program.cs
+++ task_002/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле A = m+n;
+// Выведите полученный массив на экран
+
+Console.Clear();
+Console.WriteLine("Задайте двумерный массив m x n");
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int k = 0; k < n; k++)
+    {
+        matrix[i, k] = i + k;
+        Console.Write(matrix[i, k] + " ");
+    }
+    Console.WriteLine();
+}
